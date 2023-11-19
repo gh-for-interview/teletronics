bc7915bb90ce0b858b180e7b8a1a63516396add4
alexey.naumov <alexey.naumov@revolut.com>
2023-11-19 23:09:54 +0400

    remove method

--- src/main/java/com/teletronics/assignment/model/UpdateNoteRequest.java
+++ src/main/java/com/teletronics/assignment/model/UpdateNoteRequest.java
@@ -7,7 +7,4 @@ import static java.util.UUID.randomUUID;
 
 public record UpdateNoteRequest(String title, LocalDate createdDate, String text, Optional<Tag> tag) {
 
-    public Note toNote() {
-        return new Note(randomUUID(), title, createdDate, text, tag);
-    }
 }
