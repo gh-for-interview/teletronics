e56f0c717dd8ec4749f936470fcd98b1c4606f95
alexey.naumov <alexey.naumov@revolut.com>
2023-11-20 09:28:57 +0400

    fix note checks

--- src/main/java/com/teletronics/assignment/model/Note.java
+++ src/main/java/com/teletronics/assignment/model/Note.java
@@ -9,15 +9,23 @@ import static java.lang.String.format;
 public record Note(UUID id, String title, LocalDate createdDate, String text, Optional<Tag> tag) {
     public Note(UUID id, String title, LocalDate createdDate, String text, Optional<Tag> tag) {
         this.id = checkNotNull("id", id);
-        this.title = checkNotNull("title", title);
+        this.title = checkNotEmpty("title", title);
         this.createdDate = checkNotNull("createdDate", createdDate);
-        this.text = checkNotNull("text", text);
+        this.text = checkNotEmpty("text", text);
         this.tag = checkNotNull("tag", tag);
     }
 
     private <T> T checkNotNull(String field, T value) {
         if (value == null) {
             throw new IllegalArgumentException(format("Provided value for field %s is null", field));
+        }
+        return value;
+    }
+
+    private String checkNotEmpty(String field, String value) {
+        checkNotNull(field, value);
+        if (value.isEmpty() || value.isBlank()) {
+            throw new IllegalArgumentException(format("Provided value for field %s is empty", field));
         }
         return value;
     }
